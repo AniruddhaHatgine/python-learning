7b8a26dfafe2a0d76dfcc5f9088c99912b2b7737
Aniruddha Hatgine <aniruddhahatgine@gmail.com>
2026-02-22 20:32:29 +0530

    Company Detail Type

---
+++ Company_Variable_type.py
@@ -0,0 +1,17 @@
+#variable
+
+Company = "TCS"
+Company_CEO = "Krithivasan"
+Company_Code = "TCS001"
+Company_YOY_Growth = 10.5
+Company_Dividend_Yield = 2.5
+Company_Location = "Mumbai"
+Company_Employees = 50000
+
+print(type(Company))
+print(type(Company_CEO))  
+print(type(Company_Code))
+print(type(Company_YOY_Growth))   
+print(type(Company_Dividend_Yield))
+print(type(Company_Location))
+print(type(Company_Employees))
