1b865d4a1b12a91292c1297fb22f3b4b45709af2
Aniruddha Hatgine <aniruddhahatgine@gmail.com>
2026-03-16 09:03:26 +0530

    pizza

---
+++ Pizza_Billing_System.py
@@ -0,0 +1,27 @@
+pizza_name=input("Enter the name of the pizza: ")
+pizza_size=input("Enter the size of the pizza (small, medium, large): ")
+print("How much quantity of pizza")
+quantity = int(input("Enter Value:"))
+#if user_input > 0:
+print("Accepted")
+pizza_menu = {
+    "Onion Pizza": 150,
+    "Cheese Pizza": 200,
+    "Veggie Pizza": 180,
+    "Pepperoni Pizza": 250
+}
+
+if pizza_name in pizza_menu:
+    price = pizza_menu[pizza_name]
+
+    # quantity = int(input("Enter the quantity of pizzas: "))
+    # quantity = max(1, quantity)  # Ensure quantity is at least 1
+    
+    total_cost = price * quantity
+    print(f"Pizza Name: {pizza_name}")
+    print(f"Pizza Size: {pizza_size}")
+    print(f"Quantity: {quantity}")
+    print(f"Total Cost: {total_cost}")
+
+else:
+    print("Sorry, we don't have that pizza on the menu.")   
